d2406bf4c2a1b7ffe9d85b6519922c8e0261ea19
Early Riser <tkfydtls464@gmail.com>
2024-01-15 18:35:37 +0900

    feat: 영어, 한글 글꼴 추가

--- app/[lang]/layout.jsx
+++ app/[lang]/layout.jsx
@@ -1,10 +1,59 @@
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "../globals.css";
 import Head from "next/head";
 import Header from "../@components/header";
 import Footer from "../@components/footer";
 
-const inter = Inter({ subsets: ["latin"] });
+// 한글 폰트
+const NanumSquare = localFont({
+  src: [
+    {
+      path: "../../public/fonts/NanumSquareL.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NanumSquareR.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NanumSquareB.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NanumSquareEB.otf",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+});
+// 영어 폰트
+const NanumSquareOTF = localFont({
+  src: [
+    {
+      path: "../../public/fonts/NanumSquareOTF_acL.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NanumSquareOTF_acR.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NanumSquareOTF_acB.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/NanumSquareOTF_acEB.otf",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -13,7 +62,12 @@ export const metadata = {
 
 export default function RootLayout({ children, lang }) {
   return (
-    <html lang={lang}>
+    <html
+      lang={lang}
+      className={
+        lang === "ko" ? NanumSquare.className : NanumSquareOTF.className
+      }
+    >
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
