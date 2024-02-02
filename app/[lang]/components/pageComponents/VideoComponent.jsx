da8dcae40a5c54a74df7e5770f6ff15b9ce995de
Early Riser <tkfydtls464@gmail.com>
2024-02-02 18:17:56 +0900

    fix: 1440px 이상에서 비디오 넓이 잘리던거 수정

--- app/[lang]/components/pageComponents/VideoComponent.jsx
+++ app/[lang]/components/pageComponents/VideoComponent.jsx
@@ -7,7 +7,7 @@ export default async function VideoComponent() {
             loop
             muted
             playsInline
-            className="absolute block left-1/2 top-1/2 h-full -translate-x-1/2 -translate-y-1/2 object-cover overflow-clip rounded-3xl"
+            className="absolute block left-1/2 top-1/2 w-full h-full -translate-x-1/2 -translate-y-1/2 object-cover overflow-clip rounded-3xl"
             poster="https://firebasestorage.googleapis.com/v0/b/loginform-6747a.appspot.com/o/1.webp?alt=media&token=763f4314-465e-4551-9c9c-e77012b27209"
         >
             <source
