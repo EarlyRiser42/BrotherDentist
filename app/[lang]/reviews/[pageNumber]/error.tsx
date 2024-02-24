2e9a0b8892f270ef4a2520fc4363e1a47583b407
Early Riser <tkfydtls464@gmail.com>
2024-02-24 09:49:13 +0900

    feat: error.js에 홈으로 가기버튼 추가

--- app/[lang]/reviews/[pageNumber]/error.tsx
+++ app/[lang]/reviews/[pageNumber]/error.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useRouter } from 'next/navigation';
 
 export default function Error({
     error,
@@ -7,17 +8,26 @@ export default function Error({
     error: Error & { digest?: string };
     reset: () => void;
 }) {
+    const router = useRouter();
     console.log(error);
     return (
         <div className="w-screen h-100dvh flex justify-center items-center bg-gray_800">
-            <div className="w-96/100 h-80 bg-white dark:bg-black sm:w-72 sm:h-32 rounded-3xl flex flex-col justify-evenly items-center">
+            <div className="w-96/100 h-40 bg-white dark:bg-black sm:w-72 sm:h-32 rounded-3xl flex flex-col justify-evenly items-center">
                 <h2>Something went wrong!</h2>
-                <button
-                    onClick={() => reset()}
-                    className="rounded-3xl bg-light_blue text-white p-2"
-                >
-                    Try again
-                </button>
+                <div className="flex gap-x-4">
+                    <button
+                        onClick={() => router.push('/')}
+                        className="rounded-3xl bg-black text-white px-4 py-2"
+                    >
+                        Home
+                    </button>
+                    <button
+                        onClick={() => reset()}
+                        className="rounded-3xl bg-light_blue text-white px-4 py-2"
+                    >
+                        Try again
+                    </button>
+                </div>
             </div>
         </div>
     );
