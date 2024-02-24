d314bd245f846b17f1c6ec96c956dbfa967695ef
Early Riser <tkfydtls464@gmail.com>
2024-02-24 10:47:07 +0900

    style: 소개 페이지 진료과목 디자인 변경, 이미지 css로 교체

--- components/Home/pageComponents/clinicServices.jsx
+++ components/Home/pageComponents/clinicServices.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { ArrowRight } from '@/components/Icons/Icons';
+import { ArrowRightForService } from '@/components/Icons/Icons';
 import Image from 'next/image';
-import icon from '@/public/services/3d.png';
-import support from '@/public/services/support.png';
+import seoul from '@/public/services/seoul.png';
+import yonsei from '@/public/services/yonsei.svg';
+import gov from '@/public/services/gov.png';
 import Link from 'next/link';
 
 export default function ClinicServices({ lang, page }) {
@@ -28,84 +29,84 @@ export default function ClinicServices({ lang, page }) {
                     ))}
                 </h2>
             </div>
-            <div className="grid grid-cols-10 grid-rows-10 gap-4 w-9/10 h-72 cs:h-80 sm:h-96 cmd:h-410 clg:min-w-940 clg:max-w-1250 cxl:w-85/100 c2xl:w-1900 c3xl:w-2400 lg:h-430 cxl:h-490 c2xl:h-510 c3xl:h-590">
+            <div className="grid grid-cols-10 grid-rows-10 gap-2 clg:gap-4 w-9/10 aspect-4/3 clg:min-w-940 clg:max-w-1250 cxl:w-85/100 c2xl:w-1900 c3xl:w-2400 lg:h-430 cxl:h-490 c2xl:h-510 c3xl:h-590">
                 <Link
                     href={`/${lang}/implant`}
                     className="col-span-7 row-span-6 bg-light_blue flex items-start justify-start rounded-2xl relative hover:font-bold hover:shadow-speechBalloon dark:hover:shadow-speechBalloonBlack
                 cmd:col-span-6 cmd:row-span-4"
                 >
-                    <span
-                        className="mt-2 ml-2 inline-block relative text-black dark:text-white cs:mt-4 cs:ml-4
-                        sm:text-lg lg:text-xl xl:text-2xl"
-                    >
+                    <span className="mt-2 ml-2  text-white cs:mt-4 cs:ml-4 text-base cs:text-lg sm:text-xl clg:text-2xl c2xl:text-3xl">
                         {page.home.specialties.specicalOne}
                     </span>
                     <div className="absolute right-3 bottom-3">
-                        <ArrowRight />
+                        <ArrowRightForService />
                     </div>
                 </Link>
                 <Link
                     href={`/${lang}/prosthodontics`}
-                    className="col-span-3 row-span-6 bg-blue_600 flex items-start justify-start rounded-2xl relative hover:font-bold hover:shadow-speechBalloon dark:hover:shadow-speechBalloonBlack
-                cmd:col-span-2 cmd:row-span-4"
+                    className="col-span-3 row-span-6 bg-red flex items-start justify-start rounded-2xl relative hover:font-bold hover:shadow-speechBalloon dark:hover:shadow-speechBalloonBlack
+                cmd:col-span-4 cmd:row-span-4"
                 >
-                    <span
-                        className="mt-2 ml-2 inline-block relative text-black dark:text-white cs:mt-4 cs:ml-4
-                        sm:text-lg lg:text-xl xl:text-2xl"
-                    >
+                    <span className="mt-2 ml-2  text-white cs:mt-4 cs:ml-4 text-base cs:text-lg sm:text-xl clg:text-2xl c2xl:text-3xl">
                         {page.home.specialties.specicalTwo}
                     </span>
                     <div className="absolute right-3 bottom-3">
-                        <ArrowRight />
+                        <ArrowRightForService />
                     </div>
                 </Link>
-                <div className="col-span-2 row-span-5 bg-white_600 dark:bg-black_600 hidden cmd:flex items-center justify-center rounded-2xl relative">
-                    <Image
-                        src={icon}
-                        alt="3d icon"
-                        fill
-                        sizes="(max-width: 1024px) 20vw, (max-width: 1440px) 20vw, 20vw"
-                    />
-                </div>
                 <Link
                     href={`/${lang}/laminate`}
-                    className="col-span-5 row-span-4 bg-green_600 flex items-start justify-start rounded-2xl relative hover:font-bold hover:shadow-speechBalloon dark:hover:shadow-speechBalloonBlack
-                cmd:col-span-4 cmd:row-span-4"
+                    className="col-span-5 row-span-4 bg-green_500 flex items-start justify-start rounded-2xl relative hover:font-bold hover:shadow-speechBalloon dark:hover:shadow-speechBalloonBlack
+                cmd:col-span-4 cmd:row-span-4 clg:col-span-5"
                 >
-                    <span
-                        className="mt-2 ml-2 inline-block relative text-black dark:text-white cs:mt-4 cs:ml-4
-                        sm:text-lg lg:text-xl xl:text-2xl"
-                    >
+                    <span className="mt-2 ml-2  text-white cs:mt-4 cs:ml-4 text-base cs:text-lg sm:text-xl clg:text-2xl c2xl:text-3xl">
                         {page.home.specialties.specicalThree}
                     </span>
                     <div className="absolute right-3 bottom-3">
-                        <ArrowRight />
+                        <ArrowRightForService />
                     </div>
                 </Link>
+                <div className="col-span-3 row-span-4 clg:col-span-2 border border-white_500 hidden cmd:flex flex-col justify-evenly rounded-2xl">
+                    <div className=" w-full flex items-center justify-center">
+                        <span className="font-black text-3xl clg:text-2xl cxl:text-3xl">
+                            Supported By
+                        </span>
+                    </div>
+                    <div className="w-full flex items-center justify-center">
+                        <Image
+                            src={seoul}
+                            alt="seoul logo"
+                            sizes="6rem"
+                            className="w-3/10 h-auto"
+                        />
+                        <Image
+                            src={yonsei}
+                            alt="yonsei logo"
+                            sizes="6rem"
+                            className="w-3/10 h-auto"
+                        />
+                        <Image
+                            src={gov}
+                            alt="gov logo"
+                            sizes="6rem"
+                            className="w-3/10 h-auto"
+                        />
+                    </div>
+                </div>
                 <Link
                     href={`/${lang}/cosmeticDentistry`}
-                    className="col-span-5 row-span-4 bg-white_600 flex items-start justify-start rounded-2xl relative hover:font-bold hover:shadow-speechBalloon dark:hover:shadow-speechBalloonBlack
-                cmd:col-span-4 cmd:row-span-4"
+                    className="col-span-5 row-span-4 bg-red_100 flex items-start justify-start rounded-2xl relative hover:font-bold hover:shadow-speechBalloon dark:hover:shadow-speechBalloonBlack
+                cmd:col-span-3 cmd:row-span-4"
                 >
-                    <span
-                        className="mt-2 ml-2 inline-block relative text-black dark:text-white cs:mt-4 cs:ml-4
-                        sm:text-lg lg:text-xl xl:text-2xl"
-                    >
+                    <span className="mt-2 ml-2  text-white cs:mt-4 cs:ml-4 text-base cs:text-lg sm:text-xl clg:text-2xl c2xl:text-3xl">
                         {page.home.specialties.specialFour}
                     </span>
                     <div className="absolute right-3 bottom-3">
-                        <ArrowRight />
+                        <ArrowRightForService />
                     </div>
                 </Link>
-                <div className="col-span-2 row-span-5 bg-white_600 hidden cmd:flex items-center justify-center rounded-2xl relative">
-                    <Image
-                        src={support}
-                        alt="supporters"
-                        fill
-                        sizes="(max-width: 1024px) 20vw, (max-width: 1440px) 20vw, 20vw"
-                    />
-                </div>
-                <div className="col-span-8 row-span-2 bg-white_600 dark:bg-black_600 hidden cmd:flex items-center justify-center rounded-2xl relative"></div>
+
+                <div className="col-span-10 row-span-2 bg-purple hidden cmd:flex items-center justify-center rounded-2xl relative"></div>
             </div>
         </section>
     );
